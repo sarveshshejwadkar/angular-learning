f822d19ba377c431a903e7c172a8b13df0dbdd11
Sarvesh Shejwadkar <sarveshshejwadkar@gmail.com>
2021-05-21 14:40:23 +0530

    add hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,6 +13,10 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes';  // URL to web api
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  
   constructor(
     private http: HttpClient,
     private messageService: MessageService) { }
@@ -55,6 +59,20 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_=> this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put( this.heroesUrl, hero, this.httpOptions ).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>(`updateHero`))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap( (newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
     );
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -21,4 +21,12 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
+  add(name: string):void {
+    name = name.trim();
+    if (!name) return;
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
 }
